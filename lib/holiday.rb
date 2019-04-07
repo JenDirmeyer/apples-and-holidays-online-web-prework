2c8b21be237e4be04274c08e684c08d65531f438
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-07 19:56:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,6 +103,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  list = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |key, value|
+      if value includes "BBQ"
+        list << key
+      end  
+    end
+  end  
 end
 
