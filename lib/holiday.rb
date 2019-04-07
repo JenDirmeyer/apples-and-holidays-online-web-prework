4fd93b96394edaf0fc3aadd8afad0aa3b8b3dfc1
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-07 20:04:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -108,9 +108,9 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.each do |holiday2, supplies|
       if supplies.include?("BBQ")
         list << holiday2
-        binding.pry
       end  
     end
   end  
+  list
 end
 
